cd68e6ef0d43487e77497520ca028373aee11adb
sreeharidoc <144218417+sreeharidoc@users.noreply.github.com>
2023-12-08 04:22:54 -0500

    Created 3 buttons and added a css for its style properties

--- main.js
+++ main.js
@@ -45,29 +45,67 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game(){
-    let i = 0;
-    let player_points = 0
-    let computer_points = 0
-    while (i < 5){
-        let playerSelection = prompt("Enter your choice").toLowerCase();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection))
-        if (winner === playerSelection){
-            player_points++
-        }else if (winner === computerSelection){
-            computer_points++
-        }
-        i++;
-    }
+const body = document.querySelector("body")
 
-    if (player_points > computer_points){
-        return "Player Wins! " + "Player Points:" + player_points + " Computer Points:" + computer_points
-    }else if (computer_points> player_points){
-        return "Computer Wins " + "Computer Points:" + computer_points + " Player Points:" + player_points
-    }else{
-        return "The game ends in a draw " + "Player Points:" + player_points + " Computer Points:" + computer_points
-    }
-}
+const div = document.createElement("div")
+div.classList.add("first-div")
+
+const rockButton = document.createElement("button")
+rockButton.classList.add("rock-button")
+rockButton.textContent = "Rock"
+
+const paperButton = document.createElement("button")
+paperButton.classList.add("paper-button")
+paperButton.textContent = "Paper"
+
+const scissorButton = document.createElement("button")
+scissorButton.classList.add("scissor-button")
+scissorButton.textContent = "Scissor"
+
+
+div.append(rockButton)
+div.append(paperButton)
+div.append(scissorButton)
+body.appendChild(div)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function game(){
+//     let i = 0;
+//     let player_points = 0
+//     let computer_points = 0
+//     while (i < 5){
+//         let playerSelection = prompt("Enter your choice").toLowerCase();
+//         const computerSelection = getComputerChoice();
+//         console.log(playRound(playerSelection,computerSelection))
+//         if (winner === playerSelection){
+//             player_points++
+//         }else if (winner === computerSelection){
+//             computer_points++
+//         }
+//         i++;
+//     }
+
+//     if (player_points > computer_points){
+//         return "Player Wins! " + "Player Points:" + player_points + " Computer Points:" + computer_points
+//     }else if (computer_points> player_points){
+//         return "Computer Wins " + "Computer Points:" + computer_points + " Player Points:" + player_points
+//     }else{
+//         return "The game ends in a draw " + "Player Points:" + player_points + " Computer Points:" + computer_points
+//     }
+// }
    
-console.log(game())
+// console.log(game())
